2fd11c1282e9f5245a35702b61ca3609f311c512
Sharon <tai_siewyee@hotmail.com>
2017-11-20 16:32:10 +0800

    Delete Windows Phone Application

--- MFIAlertApp/MFIAlertApp.Android/Resources/Resource.Designer.cs
+++ MFIAlertApp/MFIAlertApp.Android/Resources/Resource.Designer.cs
@@ -26,6 +26,7 @@ namespace MFIAlertApp.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::MFIAlertApp.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
